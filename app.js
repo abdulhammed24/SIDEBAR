136f70859f2e8a15740b5a752db41b95cd719d63
Abdulhammed <abdlhamd3@gmail.com>
2021-02-13 00:14:43 +0100

    new

---
+++ app.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector(".open-btn");
+const sideBar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+
+openBtn.addEventListener("click", function(){
+    sideBar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function(){
+    sideBar.classList.remove("show-sidebar");
+});
